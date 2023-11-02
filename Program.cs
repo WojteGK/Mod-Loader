de38818e79d31e6963088922ea622a753814c2f3
WojteGK <93033932+WojteGK@users.noreply.github.com>
2023-11-02 18:45:26 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MC_mods_installer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            List<string> arguments = new List<string>();
+            arguments.Add("https://www.curseforge.com/minecraft/mc-mods/torch-hit/download/4835473");
+            startInfo.Arguments = $"/C {args}";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
