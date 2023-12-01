f49305707c0a6a0fc86d85b8f809709ced226eef
WojteGK <93033932+WojteGK@users.noreply.github.com>
2023-12-01 02:32:24 +0100

    changed colors in Menu

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Security.Authentication.ExtendedProtection;
+using System.Text.RegularExpressions;
 
 namespace MC_mods_installer
 {
@@ -13,19 +14,41 @@ namespace MC_mods_installer
         protected static string UserName = Environment.UserName;
         protected static string RoamingPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
         protected static string DestinationPath = $"C:\\Users\\{UserName}\\NevaCraft";
-        public static void WriteColorLine(string text, ConsoleColor color = ConsoleColor.White)
+        protected static string ModsPath = Path.Combine(DestinationPath, "mods");
+        public static void InitializeRoaming(){
+            string NevaCraftRoamingPath = Path.Combine(RoamingPath, "NevaCraft");
+            if (!Directory.Exists(NevaCraftRoamingPath))
+            {
+                Directory.CreateDirectory(NevaCraftRoamingPath);
+            }            
+        }
+        public static void WriteColorOptions(string text, ConsoleColor textColor = ConsoleColor.White, ConsoleColor digitColor = ConsoleColor.Red)
         {
-             Console.ForegroundColor = color;
+            Regex digitPattern = new Regex(@"\d");
             int leftPadding = (Console.WindowWidth - text.Length) / 2;
-            Console.WriteLine(text.PadLeft(leftPadding + text.Length));
-            Console.ResetColor();            
-        }   
+            string paddedText = text.PadLeft(leftPadding + text.Length);
+
+            foreach (char character in paddedText)
+            {
+                if (digitPattern.IsMatch(character.ToString()))
+                {
+                    Console.ForegroundColor = digitColor;
+                }
+                else
+                {
+                    Console.ForegroundColor = textColor;
+                }
+                Console.Write(character);
+            }
+            Console.WriteLine();
+            Console.ResetColor();
+        }
         public static void DisplayMenu(bool isCleared = true)
         {
             if (isCleared) { Console.Clear(); }
-            WriteColorLine("Install mods: [1]", ConsoleColor.Green);
-            WriteColorLine("Uninstall mods:  [2]", ConsoleColor.Red);
-            WriteColorLine("Exit: [3]");
+            WriteColorOptions("[1] -- Install mods --", ConsoleColor.White, ConsoleColor.Yellow);
+            WriteColorOptions("[2] - Uninstall mods -", ConsoleColor.White, ConsoleColor.Yellow);
+            WriteColorOptions("[0] ------ Exit ------", ConsoleColor.White, ConsoleColor.Red);
             bool exit = false;         
             while(!exit)
             {
@@ -38,7 +61,7 @@ namespace MC_mods_installer
                     case '2':
                         throw new NotImplementedException();
                         break;
-                    case '3':                        
+                    case '0':                        
                         exit = true;
                         break;
                     default:
@@ -98,7 +121,7 @@ namespace MC_mods_installer
             {
                 Console.WriteLine($"Błąd deserializacji JSON: {ex.Message}");                
             }
-        }
+        }        
         static string GetExePath()
         {
             string exePath = AppDomain.CurrentDomain.BaseDirectory;
