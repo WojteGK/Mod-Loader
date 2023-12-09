6f966bb3907502bd81779e031bdce07aa7b37bb5
WojteGK <93033932+WojteGK@users.noreply.github.com>
2023-12-09 13:45:22 +0100

    added Resources.cs file

--- Resources.cs
+++ Resources.cs
@@ -5,8 +5,12 @@ namespace MC_mods_installer
 {
     public static class Resources
     {
-        public static List<Resource> Mods = new List<Resource>();
-        public static List<Resource> Shaders = new List<Resource>();
-        public static List<Resource> Textures = new List<Resource>();
+        private static List<Resource> mods = new List<Resource>();
+        private static List<Resource> shaders = new List<Resource>();
+        private static List<Resource> textures = new List<Resource>();
+
+        public static List<Resource> Mods { get => mods; set => mods = value; }
+        public static List<Resource> Shaders { get => shaders; set => shaders = value; }
+        public static List<Resource> Textures { get => textures; set => textures = value; }
     }
 } 
